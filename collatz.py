aa55a2d43db5a8ff6fe95502409077c4d880361d
duquesne419 <earleadamhotmail@gmail.com>
2017-08-28 13:18:42 -0700

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,28 @@
+def collatz(number):
+
+        while True:
+            if number == 1:
+                break
+    #        if number != 1:
+    #            continue
+            elif number % 2 == 0:
+                number = number // 2
+                print(number)
+            elif number % 2 == 1:
+                number = 3 * number + 1
+                print(number)
+            else:
+                print('You did not enter a valid number.')
+        print('Congratulations!')
+    
+def game():
+    print('Enter an interger.')
+    guess = input()
+    try:
+        guess = int(guess)
+        collatz(guess)
+    except ValueError:
+        print('You did not enter an integer.')
+        continue
+     
+game()
